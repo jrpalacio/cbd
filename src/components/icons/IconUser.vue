941aafbbd562a9a266ff2b5c19a3ed1a9df15235
JOSE REYES <jrpalacio.dev@gmail.com>
2024-08-30 14:45:06 -0600

    change IconUser

--- src/components/icons/IconUser.vue
+++ src/components/icons/IconUser.vue
@@ -15,11 +15,10 @@ export default {
     stroke-width="2"
     stroke-linecap="round"
     stroke-linejoin="round"
-    class="icon icon-tabler icons-tabler-outline icon-tabler-user-circle"
+    class="icon icon-tabler icons-tabler-outline icon-tabler-user"
   >
     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-    <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
-    <path d="M12 10m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
-    <path d="M6.168 18.849a4 4 0 0 1 3.832 -2.849h4a4 4 0 0 1 3.834 2.855" />
+    <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+    <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
   </svg>
 </template>
