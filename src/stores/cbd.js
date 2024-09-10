6db2087f02d13e45a3195143f2c9e0fff40f3dc1
JOSE REYES <jrpalacio.dev@gmail.com>
2024-09-09 18:48:40 -0600

    agregar funciones para manejo de localstorage

--- src/stores/cbd.js
+++ src/stores/cbd.js
@@ -20,6 +20,17 @@ export const useCbdStore = defineStore('cbd', () => {
     }
   }
 
+  function setTimeList(times) {
+    timeList.value = times
+  }
+
+  function addTimeLocalStorage({ id, portion, timestamp }) {
+    // Save newTime to local storage
+    const storedTimes = JSON.parse(localStorage.getItem('times')) || []
+    storedTimes.push({ id, portion, timestamp })
+    localStorage.setItem('times', JSON.stringify(storedTimes))
+  }
+
   function addTime() {
     const id = generateUniqueId()
 
@@ -28,9 +39,36 @@ export const useCbdStore = defineStore('cbd', () => {
       portion: portion.value,
       timestamp: timestamp.value
     })
+
+    addTimeLocalStorage({ id, portion: portion.value, timestamp: timestamp.value })
+
     portion.value = 1
     timestamp.value = 0
     showModal.value = false
+  }
+
+  function getStoredTimes() {
+    const storedTimes = JSON.parse(localStorage.getItem('times')) || []
+    const today = new Date().toLocaleDateString()
+    return storedTimes.filter((time) => {
+      const timeDate = new Date(time.timestamp).toLocaleDateString()
+      return timeDate === today
+    })
+  }
+
+  function getTimesByDay(day) {
+    const storedTimes = JSON.parse(localStorage.getItem('times')) || []
+    return storedTimes.filter((time) => {
+      const timeDate = new Date(time.timestamp).toLocaleDateString()
+      return timeDate === day
+    })
+  }
+  function getDataByDate(date) {
+    const storedTimes = JSON.parse(localStorage.getItem('times')) || []
+    return storedTimes.filter((time) => {
+      const timeDate = new Date(time.timestamp).toLocaleDateString()
+      return timeDate === date
+    })
   }
 
   function generateUniqueId() {
@@ -45,10 +83,29 @@ export const useCbdStore = defineStore('cbd', () => {
     return timeList.value.find((time) => time.id === id)
   }
 
+  function removeTimeLocalStorage(id) {
+    const storedTimes = JSON.parse(localStorage.getItem('times')) || []
+    const index = storedTimes.findIndex((time) => time.id === id)
+    if (index !== -1) {
+      storedTimes.splice(index, 1)
+      localStorage.setItem('times', JSON.stringify(storedTimes))
+    }
+  }
+
   function removeTime(id) {
     const index = timeList.value.findIndex((time) => time.id === id)
     if (index !== -1) {
       timeList.value.splice(index, 1)
+      removeTimeLocalStorage(id)
+    }
+  }
+
+  function updateItemLocalStorage({ id, portion }) {
+    const storedTimes = JSON.parse(localStorage.getItem('times')) || []
+    const index = storedTimes.findIndex((time) => time.id === id)
+    if (index !== -1) {
+      storedTimes[index].portion = portion
+      localStorage.setItem('times', JSON.stringify(storedTimes))
     }
   }
 
@@ -57,6 +114,7 @@ export const useCbdStore = defineStore('cbd', () => {
     if (index !== -1) {
       timeList.value[index].portion = portion
       // timeList.value[index].timestamp = timestamp.value
+      updateItemLocalStorage({ id, portion })
     }
   }
 
@@ -69,6 +127,10 @@ export const useCbdStore = defineStore('cbd', () => {
     addTime,
     getItemById,
     removeTime,
-    updateItem
+    updateItem,
+    getStoredTimes,
+    getTimesByDay,
+    getDataByDate,
+    setTimeList
   }
 })
